c3064b90f24b4510f11677717dd0b057a592b9ee
prasoon <birla.prasoon1@gmail.com>
2020-08-29 04:52:18 +0530

    Displaying the index page.

--- src/main/java/com/prasoon/apps/controllers/IndexController.java
+++ src/main/java/com/prasoon/apps/controllers/IndexController.java
@@ -1,12 +1,14 @@
 package com.prasoon.apps.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class IndexController {
 
-    @GetMapping({"/","/index",""})
+    @RequestMapping({"/","/index","","/index.html"})
     public String getIndexPage(){
         return "index";
     }
